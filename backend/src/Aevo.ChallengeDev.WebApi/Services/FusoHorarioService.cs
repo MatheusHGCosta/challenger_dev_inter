92d7bda09400a432a39db47d67a435a90b054874
Matheus Costa <matheushenriquegv8@hotmail.com>
2025-02-22 19:09:17 -0300

    Controle de fuso horário.

---
+++ backend/src/Aevo.ChallengeDev.WebApi/Services/FusoHorarioService.cs
@@ -0,0 +1,32 @@
+﻿namespace Aevo.ChallengeDev.WebApi.Services
+{
+    using System;
+    using TimeZoneConverter;
+
+    public class FusoHorarioService
+    {
+        public static DateTime ConverterFuso(DateTime dataBase, string fusoDe, string fusoPara)
+        {
+            try
+            {
+                TimeZoneInfo tzUsuario = TZConvert.GetTimeZoneInfo(fusoDe);
+                TimeZoneInfo tzSala = TZConvert.GetTimeZoneInfo(fusoPara);
+
+                DateTime horarioUtc = TimeZoneInfo.ConvertTimeToUtc(dataBase, tzUsuario);
+
+                DateTime horarioSala = TimeZoneInfo.ConvertTimeFromUtc(horarioUtc, tzSala);
+
+                return horarioSala;
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                throw new Exception("Fuso horário não encontrado.");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao converter fuso horário: {ex.Message}");
+            }
+        }
+    }
+
+}
