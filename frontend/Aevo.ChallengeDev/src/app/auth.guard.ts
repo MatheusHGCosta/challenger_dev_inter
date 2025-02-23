707074ab3525742b82897a0e6ef2977dec624364
Matheus Costa <matheushenriquegv8@hotmail.com>
2025-02-23 07:44:52 -0300

    Mensagem de validação de sessão

--- frontend/Aevo.ChallengeDev/src/app/auth.guard.ts
+++ frontend/Aevo.ChallengeDev/src/app/auth.guard.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { AlertService } from './services/alert.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private alertCtrl: AlertService
+  ) {}
 
   canActivate(): boolean {
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
-      this.router.navigate(['/login']);
+      this.alertCtrl.showError('ALERTAS.ERRO_AUTENTICAR')
+      this.router.navigate(['/']);
       return false;
     }
   }
