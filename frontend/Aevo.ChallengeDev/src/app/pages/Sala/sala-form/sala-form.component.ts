14fe4af14a771f03925dda504d0e7d111d11ebc2
Matheus Costa <matheushenriquegv8@hotmail.com>
2025-02-23 10:01:56 -0300

    Edição e criação de sala

--- frontend/Aevo.ChallengeDev/src/app/pages/Sala/sala-form/sala-form.component.ts
+++ frontend/Aevo.ChallengeDev/src/app/pages/Sala/sala-form/sala-form.component.ts
@@ -1,13 +1,94 @@
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
+import { AlertService } from '../../../services/alert.service';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SalaService } from '../../../services/sala.service';
+import { Sala } from '../../Agendamento/agendamento-form/agendamento-form.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { TimezoneDropdownComponent } from "../../../components/timezone/timezone.component";
+
+
+export interface SalaReqBody{
+  nome: string,
+  capacidade: number,
+  fusoHorario: string
+}
 
 @Component({
   selector: 'app-sala-form',
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule, FormsModule, InputTextModule, ButtonModule, RouterModule, TimezoneDropdownComponent],
   templateUrl: './sala-form.component.html',
   standalone:true,
+  providers: [TranslatePipe],
   styleUrl: './sala-form.component.scss'
 })
 export class SalaFormComponent {
+  id : string | null = '';
+  sala:Sala= {id : '',nome: '',capacidade: 0,fusoHorario: 'America/Sao_Paulo',descricao:''};
+  isEditMode = false;
+  isLoad = false
 
+   constructor(
+      private alertCtrl : AlertService,
+      private route : ActivatedRoute,
+      private router : Router,
+      private salaService : SalaService
+    ) {
+      this.route.paramMap.subscribe(params => {
+        this.id = params.get('id');
+        if (this.id) {
+          this.isEditMode = true;
+          this.salaService.getSalas().subscribe(salas => {
+            const salaExistente = salas.find(a => a.id === this.id);
+            if (salaExistente) {
+              this.sala = salaExistente
+            }
+            this.isLoad= true
+          })
+        }
+      });
+      
+    }
+
+    capturarFusoHorario(fusoHorario: string) {
+      this.sala.fusoHorario = fusoHorario;
+    }
+
+
+    salvar() {
+    
+        if (!this.sala){return;}
+        
+        
+        let salaReq : SalaReqBody = {
+          nome: this.sala.nome,
+          capacidade: this.sala.capacidade,
+          fusoHorario: this.sala.fusoHorario
+        }
+        if (this.id) {
+          this.salaService.editSala(salaReq,this.id).subscribe({
+             next: (suc) => {
+              this.alertCtrl.showSuccess('ALERTAS.SALA_EDITADA');
+              this.router.navigate(['/salas']);
+            },
+            error: (err) => {
+              this.alertCtrl.showError('ALERTAS.SALA_ERRO_EDITAR');
+            }
+          });
+        } else {
+          this.salaService.addSala(salaReq).subscribe({
+            next: () => {
+              this.alertCtrl.showSuccess('ALERTAS.SALA_CRIADA');
+              this.router.navigate(['/salas']);
+           },
+           error: () => {
+             this.alertCtrl.showError('ALERTAS.SALA_ERRO_CRIAR');
+           }
+         });
+        };
+        
+      }
 }
--- frontend/Aevo.ChallengeDev/src/app/services/sala.service.ts
+++ frontend/Aevo.ChallengeDev/src/app/services/sala.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Sala } from '../pages/Agendamento/agendamento-form/agendamento-form.component';
+import { SalaReqBody } from '../pages/Sala/sala-form/sala-form.component';
 
 
 
@@ -15,5 +16,13 @@ export class SalaService {
   }  
   deleteSala(id:string): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }  
+
+  addSala(salaBody:SalaReqBody): Observable<{salaId : string}> {
+    return this.http.post<{salaId : string}>(`${this.apiUrl}`,salaBody);
+  }  
+
+  editSala(salaBody:SalaReqBody,id:string): Observable<{salaId : string}> {
+    return this.http.put<{salaId : string}>(`${this.apiUrl}/${id}`,salaBody);
   }  
 }
